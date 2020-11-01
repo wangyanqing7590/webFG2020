7260e2f24ac16efa003813ce14541f2912d79bd7
wangyanqing7590 <wang7590@icloud.com>
2020-11-01 10:17:32 +0800

    train_set split

---
+++ dataset_split.py
@@ -0,0 +1,28 @@
+from sklearn.model_selection import train_test_split
+import os
+
+if __name__ == '__main__':
+    root = '/data1/wangyanqing/data/webFG2020_sub/'
+
+    fpath = []
+    labels = []
+    for d in os.listdir(root):
+        fd = os.path.join(root, d)
+        label = int(d)
+        for i in os.listdir(fd):
+            fp = os.path.join(fd, i)
+            fpath.append(fp)
+            labels.append(label)
+            
+    print(len(fpath), len(labels))
+    
+    x_train, x_val, y_train, y_val = train_test_split(fpath, labels, random_state=23, test_size=0.2)
+    print(len(x_train), len(x_val))
+
+    with open('train_sub.txt', 'w')as f:
+        for fn, l in zip(x_train, y_train):
+            f.write('{} {}\n'.format(fn, l))
+
+    with open('val_sub.txt', 'w')as f:
+        for fn, l in zip(x_val, y_val):
+            f.write('{} {}\n'.format(fn, l))
